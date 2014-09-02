b2255ce1872463f5462a440fdd7eded880bad077
huxl <mr.huxiaolong@gmail.com>
2014-09-02 22:22:44 +0800

    finish function load more history data with MJrefresh plugin.and load more newest data with system ele UIRefreshControl

--- Weio/Classes/Home/Controller/WBHomeViewController.m
+++ Weio/Classes/Home/Controller/WBHomeViewController.m
@@ -17,10 +17,13 @@
 #import "WBStatusFrame.h"
 #import "WBStatus.h"
 #import "WBPhoto.h"
+#import "MJRefresh.h"
 
-@interface WBHomeViewController ()
+@interface WBHomeViewController ()<MJRefreshBaseViewDelegate>
 
 @property (nonatomic,strong) NSMutableArray *statusFrames;
+@property (nonatomic,weak) UIRefreshControl *refreshControl;
+@property (nonatomic,strong) MJRefreshFooterView *refreshFootView;
 
 @end
 
@@ -32,7 +35,9 @@
 	
     [self setupNavBar];
     
-    [self setupStatusTimeLine];
+    [self setupLoadNew];
+    
+    [self setupLoadMore];
     
     self.tableView.backgroundColor = WBColor(226, 226, 226);
     self.tableView.contentInset = UIEdgeInsetsMake(0, 0, WBStatusTableBorder, 0);
@@ -40,28 +45,120 @@
 }
 
 
-// 设置 微博 TimeLine
--(void)setupStatusTimeLine{
+-(void)setupLoadMore{
+    MJRefreshFooterView *refreshFootView = [[MJRefreshFooterView alloc]init];
+    refreshFootView.delegate = self;
+    refreshFootView.scrollView = self.tableView;
+    self.refreshFootView = refreshFootView;
+}
+
+- (void)refreshViewBeginRefreshing:(MJRefreshBaseView *)refreshView{
+    [self loadMore];
+}
+
+-(void)loadMore{
     AFHTTPRequestOperationManager *man = [AFHTTPRequestOperationManager manager];
-    
     NSMutableDictionary *params = [NSMutableDictionary dictionary];
     params[@"access_token"] = [WBAccountTool account].access_token;
+    params[@"uid"] = @([WBAccountTool account].uid);
+    if(self.statusFrames){
+        WBStatusFrame *lastStatusFrame = [self.statusFrames lastObject];
+        params[@"max_id"] = lastStatusFrame.status.idstr;
+    }
     
+    // 3.发送请求
     [man GET:@"https://api.weibo.com/2/statuses/home_timeline.json" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
-//        NSLog(@"---status:---:%@",responseObject[@"pic_urls"]);
-        
         NSArray *array  =[WBStatus objectArrayWithKeyValuesArray:responseObject[@"statuses"]];
-        self.statusFrames = [NSMutableArray array];
         for(WBStatus *status in array){
             WBStatusFrame *statusFrame = [[WBStatusFrame alloc]init];
             [statusFrame setStatus:status];
             [self.statusFrames addObject:statusFrame];
         }
         [self.tableView reloadData];
+        [self.refreshFootView endRefreshing];
         
     } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-        
+        [self.refreshFootView endRefreshing];
     }];
+}
+
+-(void)setupLoadNew{
+    UIRefreshControl *con = [[UIRefreshControl alloc]init];
+    con.attributedTitle = [[NSAttributedString alloc]initWithString:@"下拉更新"];
+    [con addTarget:self action:@selector(loadNew) forControlEvents:UIControlEventValueChanged];
+    [self.tableView addSubview:con];
+    self.refreshControl = con;
+    
+    // not invoke listen method
+    [self.refreshControl beginRefreshing];
+    
+    //manual invoke listen method
+    [self loadNew];
+}
+
+
+-(void)loadNew{
+    AFHTTPRequestOperationManager *man = [AFHTTPRequestOperationManager manager];
+    NSMutableDictionary *params = [NSMutableDictionary dictionary];
+    params[@"access_token"] = [WBAccountTool account].access_token;
+    params[@"uid"] = @([WBAccountTool account].uid);
+    if(self.statusFrames){
+        WBStatusFrame *lastStatusFrame = self.statusFrames[0];
+        params[@"since_id"] = lastStatusFrame.status.idstr;
+    }
+    
+    // 3.发送请求
+    [man GET:@"https://api.weibo.com/2/statuses/home_timeline.json" parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        NSArray *array  =[WBStatus objectArrayWithKeyValuesArray:responseObject[@"statuses"]];
+        NSMutableArray *tempStatusArr = [NSMutableArray array];
+        for(WBStatus *status in array){
+            WBStatusFrame *statusFrame = [[WBStatusFrame alloc]init];
+            [statusFrame setStatus:status];
+            [tempStatusArr addObject:statusFrame];
+        }
+        int updateCount = tempStatusArr.count;
+        [tempStatusArr addObjectsFromArray:self.statusFrames];
+        self.statusFrames = tempStatusArr;
+        [self.tableView reloadData];
+        [self.refreshControl endRefreshing];
+        
+        //上面下拉一个小框 显示更新数量
+        [self displayUpdateCount:(int)updateCount];
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [self.refreshControl endRefreshing];
+    }];
+}
+
+-(void)displayUpdateCount:(int)count{
+    UIButton *countBtn = [[UIButton alloc]init];
+    countBtn.userInteractionEnabled = NO;
+    [countBtn setBackgroundImage:[UIImage imageWithName:@"timeline_new_status_background"] forState:UIControlStateNormal];
+    if(count > 0){
+        [countBtn setTitle:[NSString stringWithFormat:@"更新了%d条内容",count] forState:UIControlStateNormal];
+    }else{
+        [countBtn setTitle:@"没有新的内容" forState:UIControlStateNormal];
+    }
+    [countBtn setTitleColor:[UIColor orangeColor] forState:UIControlStateNormal];
+    countBtn.titleLabel.font = [UIFont systemFontOfSize:14];
+   
+    CGFloat btnH = 30;
+    CGFloat btnY = 64 - btnH;
+    CGFloat btnX = 2;
+    CGFloat btnW = self.view.frame.size.width - 2 * btnX;
+    countBtn.frame = CGRectMake(btnX, btnY, btnW, btnH);
+    [self.navigationController.view insertSubview:countBtn belowSubview:self.navigationController.navigationBar];
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        countBtn.transform = CGAffineTransformMakeTranslation(0, btnH + 2);
+    } completion:^(BOOL finished) {
+        [UIView animateWithDuration:0.5 delay:0.8 options:UIViewAnimationOptionCurveLinear animations:^{
+            countBtn.transform = CGAffineTransformIdentity;
+        } completion:^(BOOL finished) {
+            [countBtn removeFromSuperview];
+        }];
+    }];
+    
+    
 }
 
 
